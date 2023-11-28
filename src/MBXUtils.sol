cbca4b72303a005c654d2482c90f6ea057e0960a
bonedaddy <alex@bonedaddy.io>
2023-11-27 20:50:14 -0800

    adjust early unstake penalties to 10%/10%

--- src/MBXUtils.sol
+++ src/MBXUtils.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.0;
 library MBXUtils {
     /// @dev number of seconds in a single day
     uint256 public constant SECONDS_PER_DAY = 86400;
-    /// @dev during early unstake 5 % goes to dev
-    uint256 public constant UNSTAKE_DEV_FEE_PERCENT = 5;
-    /// @dev during early unstake 15% gets burned
-    uint256 public constant UNSTAKE_BURN_PERCENT = 15;
+    /// @dev during early unstake 10 % goes to dev
+    uint256 public constant UNSTAKE_DEV_FEE_PERCENT = 10;
+    /// @dev during early unstake 10% gets burned
+    uint256 public constant UNSTAKE_BURN_PERCENT = 10;
 
     /// @dev object contains the start and end times of an actively emitting stake pool
     struct TimeSpan {
